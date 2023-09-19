3324af3b04cd71b8a23b6bfec0792f8691c2d442
KasperPtak <kasperptak2@gmail.com>
2023-09-19 17:08:35 +0200

    Updated API-call
    Changed the api from getting gen 1 to getting gen 3 pokemon

--- lib/pokemonAPI.ts
+++ lib/pokemonAPI.ts
@@ -3,7 +3,7 @@ const POKEMON_API = "https://pokeapi.co/api/v2/"
 
 // getPokemonList -> hent alle 151 originale pokemon
 export async function getPokemonList() {
-    const response = await fetch(POKEMON_API + "pokemon?limit=151&offset=0");
+    const response = await fetch(POKEMON_API + "pokemon?limit=135&offset=251");
     const data = await response.json();
     return data.results;
     // Mulighed for at lave en typescript "interface" som definere diverse data der kommer fra API kaldet
